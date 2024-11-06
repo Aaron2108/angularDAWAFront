c3c049c17132c7ee407a85e2da71e625f3abb912
Oliver Mendez <mendezoliver2108@gmail.com>
2024-11-06 06:01:57 -0500

    Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { AlquilerComponent } from './components/alquiler/alquiler.component';
+import { CategoriasComponent } from './components/categorias/categorias.component';
+import { EstadosComponent } from './components/estados/estados.component';
+import { RolesComponent } from './components/roles/roles.component';
+import { SolicitudesComponent } from './components/solicitudes/solicitudes.component';
+import { VehiculosComponent } from './components/vehiculos/vehiculos.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+
+const routes: Routes = [
+  {path: '', redirectTo:'/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'alquiler', component: AlquilerComponent},
+  {path: 'categorias', component:CategoriasComponent },
+  {path: 'estados', component: EstadosComponent},
+  {path: 'roles', component: RolesComponent},
+  {path: 'solicitudes', component: SolicitudesComponent},
+  {path: 'vehiculos', component: VehiculosComponent},
+  {path: 'usuarios', component: UsuariosComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VehiculosComponent } from './components/vehiculos/vehiculos.component';
+import { AlquilerComponent } from './components/alquiler/alquiler.component';
+import { SolicitudesComponent } from './components/solicitudes/solicitudes.component';
+import { CategoriasComponent } from './components/categorias/categorias.component';
+import { RolesComponent } from './components/roles/roles.component';
+import { EstadosComponent } from './components/estados/estados.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { NavmenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VehiculosComponent,
+    AlquilerComponent,
+    SolicitudesComponent,
+    CategoriasComponent,
+    RolesComponent,
+    EstadosComponent,
+    UsuariosComponent,
+    NavmenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
